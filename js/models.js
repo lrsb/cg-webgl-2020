be75f0b15c0e4f682d26f36bbe47aa0a79ac70d4
lrsb <github@lorenzosiega.com>
2020-08-14 17:22:47 +0200

    bug fix (safari)

--- js/models.js
+++ js/models.js
@@ -39,8 +39,7 @@ async function loadModel(modelName) {
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR)
-        gl.generateMipmap(gl.TEXTURE_2D)
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
         gl.bindTexture(gl.TEXTURE_2D, null)
         events.onLoad--
         if (events.onLoad === 0) $('body').removeClass('loading')
